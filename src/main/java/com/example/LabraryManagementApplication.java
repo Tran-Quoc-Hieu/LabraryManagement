d34bfac261748e361e4becbd2e030bd4d1343d1a
Trần Quốc Hiếu <hieutran160120@gmail.com>
2022-02-28 18:03:52 +0700

    add project first

---
+++ src/main/java/com/example/LabraryManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabraryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabraryManagementApplication.class, args);
+	}
+
+}
